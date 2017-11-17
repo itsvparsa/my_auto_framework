322fcf8753b02835e306a2cb50187ed9e8814c7e
Vijay Parsa <vparsa@venntro.com>
2017-11-17 17:34:28 +0000

    added java alert tests all

--- features/java_s_alerts.feature
+++ features/java_s_alerts.feature
@@ -4,7 +4,22 @@ In Order to navigate to site
 I Should handle the java script alerts
 
   Scenario: Java script alert handling
-  Given I am navigated to page
-  When I click on click for JS alerts
-  Then I click ok on alert
-  And I see result on the page
+    Given I am navigated to page
+    When I click on click for JS alerts
+    Then I click ok on alert
+    And I see result on the page
+
+  Scenario: Java script alert handling
+    Given I am navigated to page
+    When I click on click for JS confirm
+    Then I click cancel on alert
+    And I see cancel result on the page
+
+  Scenario: Java script alert handling
+    Given I am navigated to page
+    When I click on click for JS prompt
+    Then I enter some text on prompt and click ok
+    And I see entered result on the page
+
+
+
--- features/step_definations/java_s_alerts_steps.rb
+++ features/step_definations/java_s_alerts_steps.rb
@@ -1,3 +1,5 @@
+# Given steps
+
 Given("I am navigated to page") do
   visit("https://the-internet.herokuapp.com")
   sleep 1
@@ -5,10 +7,22 @@ Given("I am navigated to page") do
   click_on("JavaScript Alerts")
 end
 
+# When steps
 When("I click on click for JS alerts") do
   sleep 1
   click_on("Click for JS Alert")
 end
+
+When("I click on click for JS confirm") do
+  sleep 1
+  click_on("Click for JS Confirm")
+end
+
+When("I click on click for JS prompt") do
+  sleep 1
+  click_on("Click for JS Prompt")
+end
+# Then steps
 
 Then("I click ok on alert") do
   accept_alert("I am a JS Alert")
@@ -18,3 +32,27 @@ Then("I see result on the page") do
   sleep 1
   expect(page).to have_content("You successfuly clicked an alert")
 end
+
+Then("I click cancel on alert") do
+  dismiss_confirm("I am a JS Confirm")
+end
+
+Then("I see cancel result on the page") do
+  sleep 1
+  expect(page).to have_content("You clicked: Cancel")
+end
+
+Then("I enter some text on prompt and click ok") do
+  accept_prompt(with: "Text from prompt")
+end
+
+Then("I see entered result on the page") do
+  sleep 1
+  expect(page).to have_content("You entered: Text from prompt")
+end
+
+
+
+
+
+

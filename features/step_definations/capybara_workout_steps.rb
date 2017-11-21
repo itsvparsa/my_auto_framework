9be72a3ea1b4f8f9d249ff15193802e6b0585ae2
Vijay Parsa <vparsa@venntro.com>
2017-11-21 22:04:08 +0000

    capybara heroku tests

--- features/step_definations/capybara_workout_steps.rb
+++ features/step_definations/capybara_workout_steps.rb
@@ -16,3 +16,15 @@ find("button.left").click # class
 find("button#left").click # id
 fill_in "name", with: "bob"
 click_button("Submit")
+
+Timeout.timeout(60) { sleep 1 until desktop.has_content?("Unhighlight")
+Timeout.timeout(10) { sleep 1 until desktop.home_page.has_verification_nag? == expectation }
+Timeout.timeout(10) { sleep 1 until desktop.inbox_page.has_cancel_btn? }
+
+Timeout.timeout(30) do
+  while desktop.profile_page.has_message_button?
+        sleep 1
+  end
+Timeout.timeout(10) { sleep 1 until expected_path.eql?(desktop.current_path)
+Timeout.timeout(5) { sleep 1 until desktop.has_button?("Save campaign") }
+
